72d0ed5cf6481db1b25a2472696d69515afe2a61
User <atiadjaber39@gmail.com>
2022-06-18 14:31:30 +0100

    home page progress

---
+++ webpack.mix.js
@@ -0,0 +1,36 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/easing.js', 'public/js')
+    .js('resources/js/lsb.min.js', 'public/js')
+    .js('resources/js/move-top.js', 'public/js')
+    .js('resources/js/mislider.js', 'public/js')
+    .js('resources/js/jquery.countup.js', 'public/js')
+    // .js('resources/js/jquery-2.1.4.min', 'public/js')
+    .js('resources/js/jquery.flexslider.js', 'public/js')
+    .js('resources/js/jquery.textFx.js', 'public/js')
+    .js('resources/js/jquery.transit.js', 'public/js')
+    .js('resources/js/jquery.waypoints.min.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+    ]).postCss('resources/css/style.css', 'public/css', [
+    ])
+    .postCss('resources/css/flexslider.css', 'public/css', [
+    ])
+    .postCss('resources/css/lsb.css', 'public/css', [
+    ])
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy(
+        'node_modules/@fortawesome/fontawesome-free/webfonts',
+        'public/webfonts'
+    );
